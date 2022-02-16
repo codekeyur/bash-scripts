e5e3322e770b6b818c7697d7ad7e481d5b6ffc1e
codekeyur <keyurpatel2868@gmail.com>
2022-02-16 09:12:06 -0500

    Reading File

---
+++ 30_days_of_bash/day2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+file=$1
+
+if [[ $file == "" || (! -f $file) ]]
+then 
+   echo "Using Standard Input !!"
+   file="/dev/stdin"
+fi
+
+while read -r line
+do
+   echo "$line"
+done < $file
